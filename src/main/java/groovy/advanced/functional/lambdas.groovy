b7d113568a98fe0eaeb6a5a0c0f192b238466789
we0z <weoz@ukr.net>
2018-08-24 17:41:12 +0300

    lambdas

---
+++ src/main/java/groovy/advanced/functional/lambdas.groovy
@@ -0,0 +1,14 @@
+package groovy.advanced.functional
+
+import java.util.stream.Stream
+
+[3, 1, 4, 1, 5, 9].stream()
+        .map({it * 2})
+        .filter({it % 3 == 0})
+        .forEach({println(it)})
+
+println Stream.generate(Math.&random)
+        .limit(1000)
+        .mapToInt({(int) (it * 300)})
+        .filter({it % 3 == 0})
+        .summaryStatistics()
