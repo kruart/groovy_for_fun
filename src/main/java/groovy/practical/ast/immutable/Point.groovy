0f120248849582852fa95f13d6557893c5fa7731
we0z <weoz@ukr.net>
2018-08-25 10:22:33 +0300

    ast transmormation: Immutable

---
+++ src/main/java/groovy/practical/ast/immutable/Point.groovy
@@ -0,0 +1,10 @@
+package groovy.practical.ast.immutable
+
+import groovy.transform.Immutable
+
+/**Created by kruart on 25.08.2018.*/
+@Immutable
+class Point {
+    BigDecimal x
+    BigDecimal y
+}
