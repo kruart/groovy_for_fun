701b40cb382e47dfa3d6b113a057c559538bc55c
we0z <weoz@ukr.net>
2018-08-25 10:07:57 +0300

    metaprogramming

---
+++ src/main/java/groovy/advanced/metaprogramming/CustomLevel.groovy
@@ -0,0 +1,10 @@
+package groovy.advanced.metaprogramming
+
+import groovy.transform.InheritConstructors
+
+import java.util.logging.Level
+
+/**Created by kruart on 25.08.2018.*/
+@InheritConstructors
+class CustomLevel extends Level {
+}
---
+++ src/main/java/groovy/advanced/metaprogramming/expando_demo.groovy
@@ -0,0 +1,18 @@
+package groovy.advanced.metaprogramming
+
+Expando ex = new Expando()
+ex.name = 'Fido'
+ex.speak = { String msg = 'woof' -> "$name says $msg!" }
+assert ex.speak() == 'Fido says woof!'
+assert ex.speak('bow wow') == 'Fido says bow wow!'
+
+class Cat {}
+Cat.metaClass.name = 'Garfield'
+Cat.metaClass.says = 'wants lasagna'
+Cat.metaClass.speak { -> "$name $says" }
+Cat c = new Cat()
+assert 'Garfield wants lasagna' == c.speak()
+
+c.name = 'Fluffy'
+c.says = 'says meow'
+assert c.speak() == 'Fluffy says meow'
